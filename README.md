916f48c3fb5c653bd40f6ea357707fa8b4628e0c
thomaslienbacher <lienbacher.tom@gmail.com>
2018-11-20 22:21:40 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Moving Tower
+
+A small game to help me learn Rust made using [rust-sfml](https://github.com/jeremyletang/rust-sfml).
---
+++ src/utils.rs
@@ -0,0 +1,22 @@
+use std::env;
+
+pub fn get_path(path: &str) -> String {
+    let here = {
+        match env::current_exe() {
+            Ok(p) => {
+                match p.parent() {
+                    Some(s) => { String::from(s.to_str().unwrap()) }//TODO: remove unwrap
+                    None => {
+                        panic!("Path not available {:?}", p)
+                    }
+                }
+            }
+            Err(err) => {
+                eprintln!("{}", err);
+                String::new()
+            }
+        }
+    };
+
+    (here + "/" + path)
+}
